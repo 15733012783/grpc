70ada8a2c189f338ee3a60ec9099e4533ae6cc6f
杨孙航 <13446852+yang-sunhang@user.noreply.gitee.com>
2024-03-01 15:57:20 +0800

    8

--- grpc/grpc.go
+++ grpc/grpc.go
@@ -3,22 +3,16 @@ package grpc
 import (
 	"fmt"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
 	"log"
 	"net"
 )
 
-func RegisterGRPC(port int, c func(s *grpc.Server), cret, key string) {
+func RegisterGRPC(port int, c func(s *grpc.Server)) {
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	creds, err := credentials.NewServerTLSFromFile(cret, key)
-	if err != nil {
-		log.Fatalf("failed to create credentials: %v", err)
-	}
-
-	s := grpc.NewServer(grpc.Creds(creds))
+	s := grpc.NewServer()
 	c(s)
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
